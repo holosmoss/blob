11d9f834997b84d6ae67ab0fb14abd9925086db2
aimeric <holosmoss@gmail.com>
2015-11-05 13:38:32 -0500

    debut de larbre minimax

--- src/testLab3/ArbreMinimax.java
+++ src/testLab3/ArbreMinimax.java
@@ -2,6 +2,9 @@ package testLab3;
 
 public class ArbreMinimax {
 
+	private Evaluateur eval;
+	private Generateur gene;
+
 	/**
 	(* the minimax value of n, searched to depth d *)
 	 fun minimax(n: node, d: int): int =
@@ -19,4 +22,31 @@ public class ArbreMinimax {
 	         if v' < v, v:= v'
 	      return v
 	   **/
+	protected ArbreMinimax(Evaluateur eval, Generateur gene){
+		this.eval = eval;
+		this.gene = gene;
+	}
+	/**
+	 * Récursion sur les node pour généré les prochaine nodes (gene) avec leurs valeurs (eval)
+	 * jusqu'à une profondeur d
+	 * @param node
+	 * @param d
+	 * @return value de la node
+	 */
+	protected int minimax(Node node, int d){
+		return 1;
+	}
+	
+	
+	/**
+	 * Node de l'arbre minimax
+	 * @author Holos
+	 *
+	 */
+	private class Node{
+		private Node(){
+			//TODO doit connaitre sont statut min ou max
+			//TODO doit connaitre le move qu'elle représente (changement) au board state
+		}
+	}
 }
