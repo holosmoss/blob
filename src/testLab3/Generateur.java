7ada1bf11ec55a50c5a406566e0a8d63b08c598d
aimeric <holosmoss@gmail.com>
2015-11-05 13:30:29 -0500

    lol

---
+++ src/testLab3/Generateur.java
@@ -0,0 +1,13 @@
+package testLab3;
+
+public class Generateur {
+	
+	//pour vérifier si le dernier coup était valide
+	private int[][] boardAvant = new int[8][8];	
+	private int[][] boardApres = new int[8][8];
+	
+	
+	
+	
+
+}
