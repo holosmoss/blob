4b443ba5a1c0ec9a6b3ec3e1f6fbddf9bc9a4b6e
aimeric <holosmoss@gmail.com>
2015-11-05 13:47:28 -0500

    comments sur generateur

--- src/testLab3/Generateur.java
+++ src/testLab3/Generateur.java
@@ -6,7 +6,10 @@ public class Generateur {
 	private int[][] boardAvant = new int[8][8];	
 	private int[][] boardApres = new int[8][8];
 	
-	
+	/**
+	 * Le generateur est appelé par l'arbre pour connaitre les enfants de  la node
+	 * Il evalue tout les move possible selon la position et le type de la piece ainsi que les piece voisine
+	 */
 	
 	
 
