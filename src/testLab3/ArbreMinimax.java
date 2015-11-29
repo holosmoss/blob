18d02fe7497ccf76687b1dead8332aef9ca5767b
aimeric <holosmoss@gmail.com>
2015-11-29 18:21:14 -0500

    utilise la constanteDepth Si on arrive a la fin du timer on appelle eval.LeafValue sur notre tempState.

--- src/testLab3/ArbreMinimax.java
+++ src/testLab3/ArbreMinimax.java
@@ -12,7 +12,7 @@ public class ArbreMinimax {
 	private Evaluateur eval;
 	private long startTime;
 	
-	static final int MAX_DEPTH = 6; //profondeur de recherche max plus c'est grand
+	static final int MAX_DEPTH = 9; //profondeur de recherche max plus c'est grand
 									//plus c'est précis, mais prendra plus de temps
     static final int TEMPS_MAX = 4900; // temps max en millisecondes
     //TODO this is juste for test
@@ -39,41 +39,31 @@ public class ArbreMinimax {
      */
     
     public int alphaBeta(BoardState lastState, Move move, int depth, int alpha, int beta) {
-    	/* 
-    	 * 	
-    	 * TODO use the timer to control what happens at max time
-    	 *
-    	 *
-    	 *
-    	 *TODO currentPlayer is knowable from the Move currentColor and the client ourcolor...
-    	 *
-    	 */
-		//Client.print("~~~~~~!!!!~~~~~ time elapsed  "+(System.currentTimeMillis() - startTime));
-    	if ( System.currentTimeMillis() - startTime > TEMPS_MAX ) {
-    		//TODO what do we return ?
-            //Client.print("time out "+alpha);
-    		return alpha;
-        }
-    	
-    	
     	boolean maxNode;
     	int nextColor;
 		if(move.getCurrentColor() == Client.color){
     		maxNode  = true;
     		nextColor = Client.enemyColor;
-    		//Client.print("alphabeta MAX at"+depth);
     	}else{
     		maxNode = false;
     		nextColor = Client.color;
-    		//Client.print("alphabeta MIN at"+depth);
     	}
-    	//Client.print("tempState----alphabeta--isMax : "+maxNode+"------------profondeur : "+depth);
-
-    	//System.out.println("----alphaBeta  : a="+alpha+" b="+beta+" is max "+maxNode);
+    	Client.print("----alphaBeta  : a="+alpha+" b="+beta+" is max "+maxNode+"------------profondeur : "+depth);
     	//Client.print("the received state : ");
     	//lastState.affichageGrille();
     	//generate the new BoardState for our move : and generate the moves from that boardState.
     	BoardState tempState = new BoardState(lastState,move);
+		//Client.print("~~~~~~!!!!~~~~~ time elapsed  "+(System.currentTimeMillis() - startTime));
+    	if ( System.currentTimeMillis() - startTime > TEMPS_MAX ) {
+    		//TODO what do we return ?
+            //Client.print("time out "+alpha);
+    		int leafValue = eval.leafValue(tempState);
+    		return leafValue;
+        }
+    	
+    	
+    	
+
     	//TODO pour tets
     	//Client.print("the changed state : ");
     	//tempState.affichageGrille();
@@ -83,7 +73,7 @@ public class ArbreMinimax {
     	int endingValue = eval.isGameFinish(tempState, Client.color);
     	if( endingValue != 0){
     		//we have either lost or won with this move. return its value since its a leaf
-    		Client.print("END --- "+endingValue);
+    		//Client.print("END --- "+endingValue);
     		return endingValue;
     	}
     	//verify depth
@@ -92,9 +82,10 @@ public class ArbreMinimax {
     		//evaluate current move as a leaf and return its value
     		incrementLeaf = incrementLeaf+1;
     		//System.out.println("----alphaBeta  : Leaf ! "+randomNum);
-    		//Client.print("END leaf --- "+incrementLeaf);
+    		int leafValue = eval.leafValue(tempState);
+    		//Client.print("END leaf --- "+leafValue);
     		decrementLeaf = decrementLeaf-1;
-    		return incrementLeaf;//incrementLeaf;//eval.leafValue(tempState);
+    		return leafValue;//incrementLeaf;//incrementLeaf;//
     	}
     	//we are not at the end of the tree so lets generate an other generation
     	//TODO use a test list (hardcoded moves for us and them(we need to know the active player));
@@ -109,7 +100,6 @@ public class ArbreMinimax {
     		//TODO eval should order them (differently for min max ??)
     		//we are in a max node
     		for(Move child : childs){
-    			//Client.print(child.getCurrentColor()+" color! "+depth);
     			//check for the 
     			//TODO test creating a new temp boardState for the next gen which is our parent state with the parent move applied
                 //Client.print(">>>>>>>parent depth "+depth);
@@ -117,11 +107,13 @@ public class ArbreMinimax {
     			alpha = Math.max(alpha, alphaBeta(tempState, child, depth - 1, alpha, beta) );
                 if(alpha >= beta)
                 {
-            		//Client.print("----alphaBeta  : pruned!  max "+beta);
+            		//Client.print("----alphaBeta  : pruned!  max : return ::"+beta);
                 	//TODO do we return alpha or beta ?
                     return beta;
                 }
     		}
+    		//Client.print("----alphaBeta  : return "+alpha);
+
     		return alpha;
     	}else{
     		for (Move child : childs) {
@@ -172,7 +164,7 @@ public class ArbreMinimax {
 			//TODO is the way to decide if max a true false for currentPlayer ? 
 			//TODO do we give the current State ? or a tempstate ?
 			//MAX_DEPTH
-			alpha = Math.max(alpha, alphaBeta(currentState, move, 9,  alpha,beta));
+			alpha = Math.max(alpha, alphaBeta(currentState, move, MAX_DEPTH,  alpha,beta));
 			if(alpha > bestScore)
 	        {
 	            bestMove = move;
@@ -181,7 +173,7 @@ public class ArbreMinimax {
 	        }
 			
 		}
-    	System.out.println("===================================minimax return :"+bestMove.getCurrentColor()+":"+bestMove.toString());
+    	System.out.println("===================minimax ("+bestScore+") return :"+bestMove.getCurrentColor()+":"+bestMove.toString());
 		
 		return bestMove;
 	}
